2aa6bf5a1d4d2a3c5a69a03613efe732fa1f28b7
gilanreborn <gilan.salehi@borngroup.com>
2018-12-17 17:30:47 -0500

    Style the theme color options in the nav menu

--- js/app.js
+++ js/app.js
@@ -2,15 +2,11 @@ import { q, createStore, template } from './util/reduxLite.js';
 import { rootReducer } from './reducers/root.js';
 import Nav from './components/nav.js';
 import Accordion from './components/accordion.js';
-import Flipper from './templates/flipper.js';
-import List from './templates/list.js';
-import TextSetter from './templates/text.js';
-import { html, render } from 'https://unpkg.com/lit-html@0.9.0/lit-html.js';
 
 window.q = q;
 
 window.addEventListener('load', function (e) {
-  window.app = createStore(rootReducer);
+  var provider = window.app = createStore(rootReducer);
   // accessibility:
   q('.accordion__title').map(el => el.setAttribute('tabindex', '0'));
 
@@ -18,7 +14,6 @@ window.addEventListener('load', function (e) {
   const nav = new Nav({ $container: q('.nav')[0] });
   const accordions = q('.accordion').map($container => new Accordion({ $container }));
 
-  console.log('loaded');
   q('.profile')[0].scrollIntoView({
     behavior: 'smooth',
     block: 'end',
